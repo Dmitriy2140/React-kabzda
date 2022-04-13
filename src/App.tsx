660d9107c193ded41fe15a8311dd28b225f7b213
Dmitriy2140 <dimon_shkurat@mail.ru>
2022-04-13 23:02:38 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    console.log("App rendering")
+    return (
+        <div className="App">
+            <AppTitle/>
+            <Rating/>
+            <Accordion/>
+            <Rating/>
+        </div>
+    );
+}
+
+function AppTitle() {
+    console.log("AppTitle rendering")
+    return (
+        <>This is App component!</>
+    )
+}
+
+function Rating() {
+    console.log("Rating rendering")
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+    )
+}
+
+function Star() {
+    console.log("Star rendering")
+    return (
+        <div>
+            star
+        </div>
+    )
+}
+
+function Accordion() {
+    console.log("Accordion rendering")
+    return (
+        <div>
+            <AccordionTitle/>
+            <AccordionButton/>
+        </div>
+    )
+}
+
+function AccordionTitle() {
+    console.log("AccordionTitle rendering")
+    return (
+        <div>
+            <h3>Menu</h3>
+        </div>
+    )
+}
+
+function AccordionButton() {
+    console.log("AccordionButton rendering")
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
 }
 
 export default App;
